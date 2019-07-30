bb0069c0a3bc0a7790e4da0a58e3d55fa3238b54
jmolly <roshkovskypetr@gmail.com>
2019-07-30 21:38:48 +0300

    add edit mode with save

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -5,8 +5,21 @@ export default class TodoItem extends React.Component {
   constructor() {
     super();
     this.state = {
-      isInEditMode: false
+      isInEditMode: false,
+      mouseOver: false,
     };
+  }
+
+  mouseOver = () => {
+    this.setState({
+      mouseOver: true
+    });
+  }
+
+  mouseOut = () => {
+    this.setState({
+      mouseOver: false
+    });
   }
   
   editItem = () => {
@@ -20,31 +33,31 @@ export default class TodoItem extends React.Component {
       isInEditMode: false
     });
   };
-  
+
   render() {
     let { item, toggleCompleted } = this.props;
-
+console.log(item, "item")
     return (
-      <li className="todo-list__item" key={item.key}>
+      <li className="todo-list__item" key={item.key} onMouseOver = {this.mouseOver} onMouseOut = {this.mouseOut}>
         <input
           className={item.completed ? "todo-list__text todo-list__text_completed" : "todo-list__text"}
           type="text"
-          defaultValue={item.text + item.dueDate ? ` until ${item.dueDate.replace(/T/gi, ' ')}` : ''}
+          defaultValue={item.text + (item.dueDate ? ` until ${item.dueDate.replace(/T/gi, ' ')}` : '')}
           ref="textInput"
           disabled={!this.state.isInEditMode}
         />
-        <input 
-        type="checkbox" 
-        onClick={() => toggleCompleted(item.key)}
-        >
-      </input>
-        <div>
+        <div className = {this.state.mouseOver ? "visible" : "not-visible"}>
           {this.state.isInEditMode ? (
             <button onClick={() => this.saveItem()}>save</button>
           ) : (
             <button onClick={() => this.editItem()}>edit</button>
           )}
         </div>
+        <input 
+        type="checkbox" 
+        onClick={() => toggleCompleted(item.key)}
+        >
+      </input>
       </li>
     );
   }
