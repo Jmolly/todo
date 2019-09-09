89ef769f8ff35d601617cef6b02e7f1b061ad89f
roshkovskypetr <roshkovskypetr@gmail.com>
2019-09-09 23:50:55 +0300

    add payload to actions

--- src/actions.js
+++ src/actions.js
@@ -6,28 +6,38 @@ const SAVE_TODO = "SAVE_TODO";
 
 export const addTodo = (text, id, dueDate) => ({
   type: ADD_TODO,
-  text,
-  id,
-  dueDate
+  payload: {
+    text,
+    id,
+    dueDate
+    }
 });
 
 export const toggleTodo = (id) => ({
   type: TOGGLE_TODO,
-  id
+  payload: {
+    id
+  }
 });
 
 export const deleteTodo = (id) => ({
   type: DELETE_TODO,
-  id
+  payload: {
+    id
+  }
 });
 
 export const editTodo = (id) => ({
   type: EDIT_TODO,
-  id,
+  payload: {
+    id
+  }
 });
 
 export const saveTodo = (text, id) => ({
   type: SAVE_TODO,
-  text,
-  id
+  payload: {
+    text,
+    id
+  }
 });
