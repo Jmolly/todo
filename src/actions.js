5d4779c74f2729d1833a40070c9e9f296f96b479
roshkovskypetr <roshkovskypetr@gmail.com>
2019-09-09 23:47:24 +0300

    action types in variables

--- src/actions.js
+++ src/actions.js
@@ -1,27 +1,33 @@
+const ADD_TODO = "ADD_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const DELETE_TODO = "DELETE_TODO";
+const EDIT_TODO = "EDIT_TODO";
+const SAVE_TODO = "SAVE_TODO";
+
 export const addTodo = (text, id, dueDate) => ({
-  type: "ADD_TODO",
+  type: ADD_TODO,
   text,
   id,
   dueDate
 });
 
 export const toggleTodo = (id) => ({
-  type: "TOGGLE_TODO",
+  type: TOGGLE_TODO,
   id
 });
 
 export const deleteTodo = (id) => ({
-  type: "DELETE_TODO",
+  type: DELETE_TODO,
   id
 });
 
 export const editTodo = (id) => ({
-  type: "EDIT_TODO",
+  type: EDIT_TODO,
   id,
 });
 
 export const saveTodo = (text, id) => ({
-  type: "SAVE_TODO",
+  type: SAVE_TODO,
   text,
   id
 });
